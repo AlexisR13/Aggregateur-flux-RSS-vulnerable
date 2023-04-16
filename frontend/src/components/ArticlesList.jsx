d160f639e16e3d8e035c7710a40d37646cb9f892
Alexis <alexis.richard@student-cs.fr>
2023-04-16 17:06:31 +0200

    Open article in new tab

--- frontend/src/components/ArticlesList.jsx
+++ frontend/src/components/ArticlesList.jsx
@@ -7,7 +7,9 @@ function ArticleCard({article}) {
   const date_str = date_array[2].toString() + '/' + date_array[1].toString() + '/' + date_array[0].toString()
 
   return (
-    <Link to={article.link} className='mx-12 mb-4 p-3 border flex'>
+    <Link to={article.link}
+      target="_blank" rel="noopener noreferrer"
+      className='mx-12 mb-4 p-3 border flex'>
       <div className='w-1/4 flex flex-col justify-between'>
         <h3>{article.name}</h3>
         <p className='text-xs text-gray-600'>{date_str}</p>
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Copyright } from '@mui/icons-material';
 export default function Footer() {
     return (
         <footer className='mt-20 w-full px-5 py-6 bg-zinc-300 text-center text-white'>
-            <p><Copyright sx={{ fontSize: 20 }}/> Copyright CentraleSupélec</p>
+            <p><Copyright sx={{ fontSize: 20 }}/> Copyright CentraleSupélec 2023</p>
         </footer>
     );
 }
